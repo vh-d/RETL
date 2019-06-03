255a4176171b45916e06dcd3276528c5e51896f5
vh-d <vencahaus@seznam.cz>
2019-06-03 13:36:28 +0200

    for ACCESS convert logical as VARCHAR(5)

--- R/RODBC_datatype_conv.R
+++ R/RODBC_datatype_conv.R
@@ -1,19 +1,19 @@
 # for conversion of R datatypes to a particular SQL engine's datatypes
-.RODBC_DT_CONV <- 
-  list(
-    "ACCESS" = 
-      c(
-        factor    = "VARCHAR(255)",
-        POSIXt    = "DATETIME",
-        Date      = "DATE",
-        difftime  = "TIME",
-        blob      = "BINARY",
-        integer   = "INTEGER",
-        numeric   = "DOUBLE",
-        character = "VARCHAR(255)",
-        logical   = "BIT",
-        list      = "VARCHAR(255)"
-      )
+.RODBC_DT_CONV <- new.env()
+
+.RODBC_DT_CONV[["ACCESS"]] <-  
+  c(
+    factor    = "VARCHAR(255)",
+    POSIXt    = "DATETIME",
+    Date      = "DATE",
+    difftime  = "TIME",
+    blob      = "BINARY",
+    integer   = "INTEGER",
+    numeric   = "DOUBLE",
+    character = "VARCHAR(255)",
+    logical   = "VARCHAR(5)",
+    list      = "VARCHAR(255)"
   )
+
 
 # TODO: use S3 methods a la odbc package
